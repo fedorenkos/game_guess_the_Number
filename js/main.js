5816f86d22e967d8bca87ca4f793adaa75abaeb4
Serg <fedorenkos084@gmail.com>
2020-11-29 20:58:50 +0200

    add return to IF

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
+
 function random(){
     let m = Math.random();
         m = m * 100;
         m = Math.round(m);
         console.log(m);
-    let x = 10;
         function guess(){
-            let ask = +prompt('Угадай число от 1 до 100') ;
+            let ask = +prompt('Угадай число от 1 до 100');
             let user = ask;
                 user = parseInt(user);
                 console.log(user);
                 if(user === m){
                     +confirm('Поздравляю. Вы угадали');
                     console.log(typeof(user));
+                    return guess();  
                 } else if (user > m){
                     +confirm('Загаданное число больше');
                     return guess();   
                 } else if (user < m){
                     +confirm('Загаданное число меньше');
                     return guess();   
-                }else {
-                    return alert('Игра окончена');
-                }   
+                }else if (user === isNaN){
+                    alert('Введи число');
+                } else {
+                    if(user === 0){
+                      alert('Игра окончена');  
+                    }
+                    return guess();
+                }  
         } 
         return guess();
 }

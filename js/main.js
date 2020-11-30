59e9e3ac2de233bf8c2a878e34e25736a82cada3
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-11-30 13:14:14 +0200

    сделал проверку на string и вывод алерт при нажатии отмены

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
-
-function random(){
+function random() {
     let m = Math.random();
-        m = m * 100;
-        m = Math.round(m);
-        console.log(m);
-        function guess(){
-            let ask = +prompt('Угадай число от 1 до 100');
-            let user = ask;
-                user = parseInt(user);
-                console.log(user);
-                if(user === m){
-                    +confirm('Поздравляю. Вы угадали');
-                    console.log(typeof(user));
-                    return guess();  
-                } else if (user > m){
-                    +confirm('Загаданное число больше');
-                    return guess();   
-                } else if (user < m){
-                    +confirm('Загаданное число меньше');
-                    return guess();   
-                }else if (user === isNaN){
-                    alert('Введи число');
-                } else {
-                    if(user === 0){
-                      alert('Игра окончена');  
-                    }
-                    return guess();
-                }  
-        } 
-        return guess();
+    m = m * 100;
+    m = Math.round(m);
+    console.log(m);
+
+    function guess() {
+        let ask = prompt('Угадай число от 1 до 100');
+        let user = ask;
+        user = parseInt(user);
+        if (user === 0) {
+            alert('Игра окончена');
+            return guess();
+        }
+        if (isNaN(user)) {
+            console.log(typeof(user));
+            alert('Введи число');
+        }
+        console.log(user);
+        if (user === m) {
+            confirm('Поздравляю. Вы угадали');
+            console.log(typeof(user));
+        } else if (user > m) {
+            confirm('Загаданное число больше');
+            return guess();
+        } else if (user < m) {
+            confirm('Загаданное число меньше');
+            return guess();
+        } else {
+            return guess();
+        }
+    }
+    return guess();
 }
 random();

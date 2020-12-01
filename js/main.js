a3cbaa07cd9488c850dd746bca5be70dbab81934
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-12-01 10:31:38 +0200

    поменял рандом, присвоил только одну переменную ask, убрал return от функции guess, поменял условия на результат веденного числа

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
 function random() {
-    let m = Math.random();
-    m = m * 100;
-    m = Math.round(m);
-    console.log(m);
+    let rand = Math.random() * 100;
+    rand = Math.round(rand);
+    console.log('Число - ' + rand);
 
     function guess() {
         let ask = prompt('Угадай число от 1 до 100');
-        let user = ask;
-        user = parseInt(user);
-        if (user === 0) {
+        ask = parseInt(ask);
+        if (ask === 0) {
             alert('Игра окончена');
             return guess();
         }
-        if (isNaN(user)) {
-            console.log(typeof(user));
+        if (isNaN(ask)) {
+            console.log(typeof(ask));
             alert('Введи число');
         }
-        console.log(user);
-        if (user === m) {
-            confirm('Поздравляю. Вы угадали');
-            console.log(typeof(user));
-        } else if (user > m) {
-            confirm('Загаданное число больше');
-            return guess();
-        } else if (user < m) {
+        console.log(ask);
+        if (ask === rand) {
+            alert('Поздравляю. Вы угадали');
+            console.log(typeof(ask));
+        } else if (ask > rand) {
             confirm('Загаданное число меньше');
-            return guess();
+            guess();
+        } else if (ask < rand) {
+            confirm('Загаданное число больше');
+            guess();
         } else {
-            return guess();
+            guess();
         }
     }
     return guess();

5393f1ed6b37fc066c3144f6ba9167d1b8f9c412
Serhiy Fedorenko <fedorenkos084@gmail.com>
2020-12-04 15:41:19 +0200

    исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function random() {
     let m = Math.random();
     m = m * 100;
     m = Math.round(m);
-    console.log(m);
+    console.log('Число: ' + m);
 
     function guess() {
         let ask = prompt('Угадай число от 1 до 100');
-        let user = ask;
-        user = parseInt(user);
-        if (user === 0) {
+        if (ask === null) {
             alert('Игра окончена');
-            return guess();
+            return;
         }
-        if (isNaN(user)) {
-            console.log(typeof(user));
+        if (!isNumber(ask)) {
+            console.log(typeof(ask));
             alert('Введи число');
         }
-        console.log(user);
-        if (user === m) {
+        console.log(ask);
+        if (ask === m) {
             confirm('Поздравляю. Вы угадали');
-            console.log(typeof(user));
-        } else if (user > m) {
+            console.log(typeof(ask));
+        } else if (ask > m) {
             confirm('Загаданное число больше');
             return guess();
-        } else if (user < m) {
+        } else if (ask < m) {
             confirm('Загаданное число меньше');
             return guess();
         } else {
